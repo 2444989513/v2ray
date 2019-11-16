29ca125da6a32c247786456ad804a2ccb8c91cbc
2444989513 <56295510+2444989513@users.noreply.github.com>
2019-11-16 21:48:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -360,8 +360,8 @@ nginx_conf_add(){
     touch ${nginx_conf_dir}/v2ray.conf
     cat>${nginx_conf_dir}/v2ray.conf<<EOF
     server {
-        listen 80;
-        listen [::]:80;
+        listen 80 ssl;
+        listen [::]:80 ssl;
         
         ssl_certificate       /data/v2ray.crt;
         ssl_certificate_key   /data/v2ray.key;
