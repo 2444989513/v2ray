c325e6aab0e1ad5324204535032863df0d74c57e
2444989513 <56295510+2444989513@users.noreply.github.com>
2019-11-20 15:21:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -360,14 +360,13 @@ nginx_conf_add(){
     touch ${nginx_conf_dir}/v2ray.conf
     cat>${nginx_conf_dir}/v2ray.conf<<EOF
     server {
-        listen 80;
         listen 443 ssl;
-        listen [::]:80;
         listen [::]:443 ssl;
         ssl_certificate       /data/v2ray.crt;
         ssl_certificate_key   /data/v2ray.key;
         ssl_protocols         TLSv1.3;
         ssl_ciphers           TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
         server_name           serveraddr.com;
         index index.html index.htm;
         root  /home/wwwroot/250;
@@ -384,14 +383,10 @@ nginx_conf_add(){
 }
     server {
         listen 80;
-        listen 443 ssl;
         listen [::]:80;
-        listen [::]:443 ssl;
         server_name serveraddr.com;
         return 301 https://use.shadowsocksr.win\$request_uri;
         
-        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
-
     }
 EOF
 
